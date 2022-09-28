79ef71ce0c2e56e4dae1bba70ca82ed8ec81df4d
Gladiator <101484326+AntoniouIoannis@users.noreply.github.com>
2022-09-28 08:44:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import pyqrcode
 from PIL import Image
 import time
-link = input('Enter anything to qenerate QR: ')
+link = input('Εισαγάγετε οτιδήποτε για να δημιουργήσετε το QR: ')
 qr_code = pyqrcode.create(link)
 qr_code.png("QRCode.png", scale=6)
 Image.open("QRCode.png")
 print('processing...')
 time.sleep (5)
-print('BarCode completed.')
+print('Το barcode δημιουργήθηκε\nΑναζητήστε την φωτο QRCode.png στον κατάλογο σας.')
